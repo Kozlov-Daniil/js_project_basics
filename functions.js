5c66f46d1c92d5ee8da28aba0505114f85e1debc
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2022-10-27 02:24:41 +0300

    functions_basic2.0

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 //Функции
 
-// function declaration
+//1 function declaration
 function greet(name) {
     console.log('Привет - ', name)
 }
@@ -18,13 +18,75 @@ console.log(typeof greet)
 
 console.dir(greet)
 
-//анонимные функции
+//2 анонимные функции
 
-let counter = 0
-const interval = setInterval(function(){
-    if (counter === 5){
-    clearInterval(interval)
-    } else{
-    console.log(++counter)
-        }
-}, 1000)
+//let counter = 0
+//const interval = setInterval(function(){
+//    if (counter === 5){
+//    clearInterval(interval)
+//    } else{
+//    console.log(++counter)
+//        }
+//}, 1000)
+
+// 3 стрелочные функции
+
+function greet(name) {
+    console.log('Привет - ', name)
+}
+
+const arrow = (name) => {
+    console.log('Привет - ', name)
+}
+
+const arrow2 = name => console.log('Привет - ', name)
+
+arrow('Eagle')
+arrow2('Eagle2')
+
+const pow2 = num => {
+    return num ** 2
+}
+
+const Pow2 = num => num ** 2
+
+console.log(pow2(5))
+console.log(Pow2(7))
+
+// 4 Параметры по умолчанию
+
+const sum = (a, b) => a + b
+
+console.log(sum(40, 5))
+
+
+const sum2 = (a, b) => a + b
+
+console.log(sum2(40))
+
+const sum3 = (a, b=3) => a + b
+
+console.log(sum3(40))
+
+function sumAll(...all) {
+    let result = 0
+    for (let num of all){
+        result += num
+    }
+    return result
+}
+
+const res = sumAll(1, 2, 3, 4, 5)
+console.log(res)
+
+// 5 Замыкания
+
+function createMember(name) {
+    return function(lastName) {
+        console.log(name + lastName)
+    }
+}
+
+const logWithLastName = createMember('Daniil')
+console.log(logWithLastName('Kozlov'))
+console.log(logWithLastName('Orlov'))
