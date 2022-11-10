67286475a89c594fda103ed6db5d68d2afbb415c
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2022-11-10 03:32:18 +0300

    check_js_v3

--- check.js
+++ check.js
@@ -103,4 +103,156 @@ obj.age=21
 delete obj.name
 console.log(obj)
 
+//Closures
+
+function sayHelloTo(name){
+    const message = 'Hello ' + name
+    
+    return function() {
+        console.log(message)
+    }
+}
+
+
+const helloToElena = sayHelloTo('Elena')
+helloToElena()
+
+
+function createFrameworkManager() {
+    const fw = ['Angular', 'React']
+    
+    return{
+        print: function() {
+            console.log(fw.join(' '))
+        },
+        add: function (framework){
+            fw.push(framework)
+        }
+    }
+}
+
+const manager = createFrameworkManager()
+console.log(manager)
+manager.print()
+manager.add('VueJS')
+manager.print()
+
+//SETTIMEOUT
+
+const fib = [1, 2, 3, 5, 8, 13]
+
+for (let i = 0; i< fib.length; i++){
+    (function(j){
+         setTimeout(function(){
+        console.log(`fib[${j}] = ${fib[j]}`)
+    }, 1500)
+    })(i)
+   
+}
+
+
+//IIFE - immediate invoked function express
+
+let result = []
+//for (var i = 0; i < 5; i++){
+//    result.push(function(){
+//        console.log(i)
+//    })
+//}
+//
+//result[2]()
+//result[4]()
+
+for (var i = 0; i < 5; i++){
+    (function(){
+        var j = i
+        result.push(function(){console.log(j)})
+    })()
+}
+
+result[2]()
+result[4]()
+
+//CONTEXT
+
+const person = {
+    surname: 'Старк',
+    knows: function(what, name){
+        console.log(`Ты ${what} знаешь, ${name} ${this.surname}`)
+    }
+}
+
+const john = { surname: 'Сноу'}
+
+person.knows('всё', 'Бран')
+person.knows.call(john, 'ничего не', 'Джон')
+person.knows.apply(john, ['ничего не', 'Джон'])
+person.knows.call(john, ...['ничего не', 'Джон'])
+person.knows.bind(john, 'ничего не', 'Джон')()
+
+
+function Person(name, age){
+    this.name = name
+    this.age = age
+    
+    console.log(this)
+}
+
+const elena = new Person('Elena', 20)
+
+
+
+//Явный
+function logThis() {
+    console.log(this)
+}
+
+const objc = {num: 42}
+logThis.apply(objc)
+logThis.call(objc)
+logThis.bind(objc)()
+
+//Не явный
+const animal = {
+    legs: 4,
+    logThis: function() {
+        console.log(this)
+    }
+}
+
+animal.logThis()
+
+
+function cat(color){
+    this.color = color
+    console.log('This', this)
+    ;(() => console.log('Arrow this', this))()
+}
+
+new cat ('red')
+
+
+//NEW
+
+function Cat (color, name){
+    this.color = color
+    this.name = name
+}
+
+const Cats = new Cat('black', 'Cit')
+console.log(Cats)
+
+function MyNew(constructor, ...args){
+    const objt = {}
+    Object.setPrototypeOf(objt, constructor.prototype)
+    return constructor.apply(objt, args) || objt
+}
+
+const kit = MyNew(Cat, 'gray', 'Kot')
+console.log(kit)
+
+
+//PROTOTYPES
+// __proto__
+
 
